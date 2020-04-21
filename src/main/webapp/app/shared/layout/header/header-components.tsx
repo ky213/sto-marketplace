b0079e847db0577f07a1f2a4ba60c5e87e4c5b15
ky <y.keddaoui@gmail.com>
2020-04-21 21:33:40 +0100

    changed theme colors and added alpinetech logos

--- src/main/webapp/app/shared/layout/header/header-components.tsx
+++ src/main/webapp/app/shared/layout/header/header-components.tsx
@@ -8,14 +8,14 @@ import appConfig from 'app/config/constants';
 
 export const BrandIcon = props => (
   <div {...props} className="brand-icon">
-    <img src="content/images/logo-jhipster.png" alt="Logo" />
+    <img src="content/images/alpinetech_logo.svg" alt="Logo" />
   </div>
 );
 
 export const Brand = props => (
   <NavbarBrand tag={Link} to="/" className="brand-logo">
     <BrandIcon />
-    <span className="brand-title">Exchange</span>
+    {/* <span className="brand-title">Exchange</span> */}
     <span className="navbar-version">{appConfig.VERSION}</span>
   </NavbarBrand>
 );
