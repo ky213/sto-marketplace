d15c931818f99bf3f39807b476694d4aa32f1e96
ky <y.keddaoui@gmail.com>
2020-04-28 17:28:16 +0100

    added users routes

--- src/main/webapp/app/modules/dashboard/pages/users/index.tsx
+++ src/main/webapp/app/modules/dashboard/pages/users/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Switch } from 'react-router-dom';
 
-const Users = () => <h1>Users</h1>;
+import UserSettings from 'app/entities/user-setting';
+import PrivateRoute from 'app/shared/auth/private-route';
+
+const Users = () => (
+  <div className="bg-white p-2 ">
+    <Switch>
+      <PrivateRoute component={UserSettings} />
+    </Switch>
+  </div>
+);
 
 export default Users;
