ea297497ddc53cafa4f042e4ab0f818ca031c544
ky <y.keddaoui@gmail.com>
2020-04-30 14:59:17 +0100

    changed login screen

--- src/main/webapp/app/modules/dashboard/pages/bank-info/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const BankInfo = () => <h1>Bank Info</h1>;
-
-export default BankInfo;
--- src/main/webapp/app/modules/dashboard/pages/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as DashboardHome } from './home';
-export { default as BankInfo } from './bank-info';
-export { default as Orders } from './orders';
-export { default as Reports } from './reporting';
-export { default as SecurityToken } from './security-token';
-export { default as Users } from './users';
-export { default as Settings } from './settings';
