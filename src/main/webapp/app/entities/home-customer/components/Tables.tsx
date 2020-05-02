5c9a054e77b050a93c25c65b1add38bc1372a420
ky <y.keddaoui@gmail.com>
2020-05-02 16:41:50 +0100

    third table

--- src/main/webapp/app/entities/home-customer/components/Tables.tsx
+++ src/main/webapp/app/entities/home-customer/components/Tables.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Table, Card, CardHeader, CardBody, CardFooter, Button } from 'reactstrap';
+import { Row, Table, Card, CardHeader, CardBody, CardFooter, Button, Badge } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Tables = () => {
@@ -173,6 +173,83 @@ const Tables = () => {
                 <td>7</td>
                 <td>1250 CHF</td>
                 <td>8750.50 CHF</td>
+              </tr>
+            </tbody>
+          </Table>
+        </CardBody>
+        <CardFooter className="d-flex p-0">
+          <Button className="ml-auto" color="none ">
+            <span className=" mr-2" style={{ fontSize: '14px' }}>
+              view all
+            </span>
+            <FontAwesomeIcon icon="caret-right" />
+          </Button>
+        </CardFooter>
+      </Card>
+      <Card className="p-0 mt-3 mx-auto ml-3 col-12 ">
+        <CardBody className="p-0">
+          <Table>
+            <thead>
+              <tr className="border-0 bg-beige">
+                <th>Order ref</th>
+                <th>Token</th>
+                <th>Symbol</th>
+                <th>Category</th>
+                <th>Type</th>
+                <th>Volume</th>
+                <th>Price</th>
+                <th>Total</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  ODR43897{' '}
+                  <Badge color="none" className="btn btn-outline-success">
+                    open
+                  </Badge>
+                </td>
+                <td>AlKAI</td>
+                <td>ALK</td>
+                <td>Equity</td>
+                <td>BUY</td>
+                <td>3000</td>
+                <td>12 CHF</td>
+                <td>36000 CHF</td>
+                <td>{new Date().toLocaleString()}</td>
+              </tr>
+              <tr>
+                <td>
+                  ORD23456{' '}
+                  <Badge color="none" className="btn btn-outline-success">
+                    open
+                  </Badge>
+                </td>
+                <td>Xgeneva</td>
+                <td>XGE</td>
+                <td>Real Estate</td>
+                <td>SELL</td>
+                <td>2000</td>
+                <td>18 CHF</td>
+                <td>36000 CHF</td>
+                <td>{new Date().toLocaleString()}</td>
+              </tr>
+              <tr>
+                <td>
+                  ODR43234{' '}
+                  <Badge color="none" className="btn btn-outline-warning">
+                    Pending
+                  </Badge>
+                </td>
+                <td>Xgeneva</td>
+                <td>XGE</td>
+                <td>Real Estate</td>
+                <td>BUY</td>
+                <td>200</td>
+                <td>8 CHF</td>
+                <td>16000 CHF</td>
+                <td>{new Date().toLocaleString()}</td>
               </tr>
             </tbody>
           </Table>
