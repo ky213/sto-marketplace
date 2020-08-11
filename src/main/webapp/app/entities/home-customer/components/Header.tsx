6d8fcdf4c22df0bbbefffd2ac9d15f1a0137a2ff
ky <y.keddaoui@gmail.com>
2020-08-11 08:20:11 +0100

    home-customer fixes

--- src/main/webapp/app/entities/home-customer/components/Header.tsx
+++ src/main/webapp/app/entities/home-customer/components/Header.tsx
@@ -3,17 +3,18 @@ import { Row, Col, Card, CardTitle, CardBody } from 'reactstrap';
 import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import { getTotalSTAmount, getBankAccountBalance } from '../home-customer.reducer';
+import { getTotalSTAmount, getTopTotalSTAmounts, getBankAccountBalance } from '../home-customer.reducer';
 import { IRootState } from 'app/shared/reducers';
 import 'app/shared/components/home/card-header.scss';
 
 export interface HeaderProps extends StateProps, DispatchProps {}
 
 const Header = (props: HeaderProps) => {
-  const { user, totalSTAmount, bankAccountBalance } = props;
+  const { user, totalSTAmount, topTotalSTAmount, bankAccountBalance } = props;
 
   useEffect(() => {
     props.getTotalSTAmount(user.id);
+    props.getTopTotalSTAmounts(user.id);
     props.getBankAccountBalance(user.login);
   }, []);
 
@@ -30,7 +31,7 @@ const Header = (props: HeaderProps) => {
                 <h6 className="font-weight-bold mb-0 text-primary">
                   CHF{' '}
                   {Object.values(totalSTAmount)
-                    .reduce((t: number, v: number) => t + v, 0.0)
+                    ?.reduce((v, s) => v + s, 0)
                     .toLocaleString()}
                 </h6>
               </Col>
@@ -43,29 +44,27 @@ const Header = (props: HeaderProps) => {
           </CardBody>
         </Card>
       </Col>
-      {Object.keys(totalSTAmount)
-        .slice(0, 1)
-        .map((k, i) => (
-          <Col className="px-1" key={i}>
-            <Card className="px-0 py-2 mb-4 mb-xl-0">
-              <CardBody className="py-2">
-                <Row>
-                  <Col>
-                    <CardTitle tag="p" className="text-muted mb-1">
-                      {k}
-                    </CardTitle>
-                    <h6 className="font-weight-bold mb-0 text-primary">CHF {totalSTAmount[k]?.toLocaleString()}</h6>
-                  </Col>
-                  <Col className="col-3 p-0">
-                    <div className="icon ml-auto mr-1 bg-success  rounded-circle shadow">
-                      <FontAwesomeIcon icon="chart-bar" color="white" />
-                    </div>
-                  </Col>
-                </Row>
-              </CardBody>
-            </Card>
-          </Col>
-        ))}
+      {topTotalSTAmount.slice(0, 2).map((el, i) => (
+        <Col className="px-1" key={i}>
+          <Card className="px-0 py-2 mb-4 mb-xl-0">
+            <CardBody className="py-2">
+              <Row>
+                <Col>
+                  <CardTitle tag="p" className="text-muted mb-1">
+                    {el?.symbol}
+                  </CardTitle>
+                  <h6 className="font-weight-bold mb-0 text-primary">CHF {el?.totalAmount?.toLocaleString()}</h6>
+                </Col>
+                <Col className="col-3 p-0">
+                  <div className="icon ml-auto mr-1 bg-success  rounded-circle shadow">
+                    <FontAwesomeIcon icon="chart-bar" color="white" />
+                  </div>
+                </Col>
+              </Row>
+            </CardBody>
+          </Card>
+        </Col>
+      ))}
       <Col className="px-1">
         <Card className="px-0 py-2 mb-4 mb-xl-0 bg-primary">
           <CardBody className="py-2 ">
@@ -96,11 +95,13 @@ const Header = (props: HeaderProps) => {
 const mapStateToProps = ({ homeCustomer, authentication }: IRootState) => ({
   user: authentication.account,
   totalSTAmount: homeCustomer.totalSTAmounts,
+  topTotalSTAmount: homeCustomer.topTotalSTAmounts,
   bankAccountBalance: homeCustomer.bankAccountBalance
 });
 
 const mapDispatchToProps = {
   getTotalSTAmount,
+  getTopTotalSTAmounts,
   getBankAccountBalance
 };
 
